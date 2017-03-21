b559e25dbafda5bc40496309cd69cba8664ca296
Chicker <vadim.agishev@gmail.com>
2017-03-21 21:31:51 +0500

    Add validation

--- src/main/java/ru/chicker/ehcache/dto/CountryInformationRequestDto.java
+++ src/main/java/ru/chicker/ehcache/dto/CountryInformationRequestDto.java
@@ -1,6 +1,12 @@
 package ru.chicker.ehcache.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CountryInformationRequestDto {
+    @NotNull
+    @Min(7)
     private String ipAddress;
 
     public CountryInformationRequestDto() {
--- src/main/java/ru/chicker/ehcache/web/controllers/CountryDeterminerController.java
+++ src/main/java/ru/chicker/ehcache/web/controllers/CountryDeterminerController.java
@@ -3,6 +3,7 @@ package ru.chicker.ehcache.web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.chicker.ehcache.dto.CountryInformationDto;
 import ru.chicker.ehcache.dto.CountryInformationRequestDto;
